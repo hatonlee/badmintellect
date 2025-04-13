e8c69152d16261b84dda6317675f331fe0070c08
hatonlee <hatonen.leevi@gmail.com>
2025-04-13 22:17:12 +0300

    Style: Improved SQL readability

--- res_handler.py
+++ res_handler.py
@@ -10,17 +10,22 @@ def get_reservations(r_id="%", r_title="%", r_start_time="%", r_end_time="%", r_
 
 # add a new reservation into the database
 def add_reservation(title, start_time, end_time, place, user_id):
-    sql = "INSERT INTO reservations (title, start_time, end_time, place, user_id) VALUES (?, ?, ?, ?, ?)"
+    sql = """INSERT INTO reservations (title, start_time, end_time, place, user_id)
+             VALUES (?, ?, ?, ?, ?)"""
     db.execute(sql, [title, start_time, end_time, place, user_id])
+
     reservation_id = db.last_insert_id()
     return reservation_id
 
 # update a reservation with new values
 def update_reservation(reservation_id, new_title, new_start_time, new_end_time, new_place):
-    sql = "UPDATE reservations SET title = ?, start_time = ?, end_time = ?, place = ? WHERE id = ?"
+    sql = """UPDATE reservations
+             SET title = ?, start_time = ?, end_time = ?, place = ?
+             WHERE id = ?"""
     db.execute(sql, [new_title, new_start_time, new_end_time, new_place, reservation_id])
 
 # remove a reservation with a specified id
 def remove_reservation(reservation_id):
-    sql = "DELETE FROM reservations WHERE id = ?"
+    sql = """DELETE FROM reservations
+             WHERE id = ?"""
     db.execute(sql, [reservation_id])
