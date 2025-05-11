f8c18df567dc730386cb31fb41d6d4b3268a8f0a
hatonlee <hatonen.leevi@gmail.com>
2025-05-11 17:16:15 +0300

    test: added testdata for tags

---
+++ init.sql
@@ -0,0 +1,7 @@
+INSERT INTO allowed_tags (tag) VALUES ("beginner");
+INSERT INTO allowed_tags (tag) VALUES ("intermediate");
+INSERT INTO allowed_tags (tag) VALUES ("advanced");
+INSERT INTO allowed_tags (tag) VALUES ("expert");
+INSERT INTO allowed_tags (tag) VALUES ("professional");
+INSERT INTO allowed_tags (tag) VALUES ("casual");
+INSERT INTO allowed_tags (tag) VALUES ("competitive");
