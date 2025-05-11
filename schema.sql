80a020ae4401d36da369bb1e4258532881c11720
hatonlee <hatonen.leevi@gmail.com>
2025-05-11 17:11:35 +0300

    Feat: Added enrollments
    Enrollments keep track of users' enrollments.
    Added/changed columns of other tables for future features.

--- schema.sql
+++ schema.sql
@@ -1,32 +1,64 @@
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS allowed_tags;
+DROP TABLE IF EXISTS enrollments;
+DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS users;
+
+DROP INDEX IF EXISTS idx_reservations_user_id;
+DROP INDEX IF EXISTS idx_reservations_title;
+DROP INDEX IF EXISTS idx_reservations_place;
+DROP INDEX IF EXISTS idx_reservations_date;
+DROP INDEX IF EXISTS idx_reservations_time;
+DROP INDEX IF EXISTS idx_reservations_duration;
+
+DROP INDEX IF EXISTS idx_tags_reservation_id;
+DROP INDEX IF EXISTS idx_tags_tag;
+
+DROP INDEX IF EXISTS idx_comments_reservation_id;
+DROP INDEX IF EXISTS idx_comments_user_id;
+
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
+    user_role, TEXT,
     password_hash TEXT,
-    image BLOB
+    profile_picture BLOB
 );
 
 CREATE TABLE reservations (
-    id INTEGER PRIMARY KEY,
+    reservation_id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
     title TEXT,
-    start_time TEXT,
-    end_time TEXT,
     place TEXT,
-    user_id INTEGER REFERENCES users
+    date TEXT,
+    time TEXT,
+    duration TEXT
 );
 
 CREATE TABLE tags (
-    id INTEGER PRIMARY KEY,
-    tag TEXT,
-    reservation_id INTEGER REFERENCES reservations
+    tag_id INTEGER PRIMARY KEY,
+    reservation_id INTEGER REFERENCES reservations,
+    tag TEXT
 );
 
 CREATE TABLE allowed_tags (
-    id INTEGER PRIMARY KEY,
+    allowed_tag_id INTEGER PRIMARY KEY,
     tag TEXT UNIQUE
 );
 
 CREATE TABLE comments (
-    id INTEGER PRIMARY KEY,
+    comment_id INTEGER PRIMARY KEY,
+    user_id TEXT REFERENCES users,
+    reservation_id INTEGER REFERENCES reservations,
     comment TEXT,
-    reservation_id INTEGER REFERENCES reservations
-)
+    post_time TEXT
+);
+
+CREATE TABLE enrollments (
+    user_id INTEGER,
+    reservation_id INTEGER,
+    PRIMARY KEY (user_id, reservation_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id)
+);
